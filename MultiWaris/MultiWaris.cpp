9f2323f839f0395b5d0eca776aa65a878a0cf391
Kakonoomoide <rizkiramada128@gmail.com>
2023-05-31 20:28:56 +0700

    membuat class pelajar

--- MultiWaris/MultiWaris.cpp
+++ MultiWaris/MultiWaris.cpp
@@ -14,7 +14,34 @@ public:
 	}
 };
 
+class manusia {
+public:
+	string jeniskelamin;
 
+	manusia(string pJenisKelamin) : jeniskelamin(pJenisKelamin) {
+		cout << "Orang dibuat\n" << endl;
+	}
+
+	~manusia() {
+		cout << "Orang dihapus\n" << endl;
+	}
+};
+
+class pelajar : private manusia, public orang {
+public:
+	string sekolah;
+
+	pelajar(string pNama, string pJenisKelamin, string pSekolah) : 
+		orang(pNama), manusia(pJenisKelamin), sekolah(pSekolah) {
+		cout << "Pelajar dibuat\n" << endl;
+	}
+	~pelajar(){
+		cout << "Pelajar dihapus\n" << endl;
+	}
+	string perkenalan() {
+		return " Hallo, nama saya " + nama + " dengan jenis kelamin " + jeniskelamin + " dari sekolah" + sekolah + "\n\n";
+	}
+};
 
 int main(){
 	
